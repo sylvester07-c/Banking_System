a77ad5ce8ee93ec8485882384eb4f6f60ae29d81
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-04-21 17:34:43 +0100

    Updates

--- src/Branch.java
+++ src/Branch.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class Branch {
+
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return  customers;
+    }
+
+    public boolean newCustomer(String name, double transaction) {
+        Customer customer = new Customer(name, transaction);
+        if(customers.add(customer)){
+            return true;
+        }
+        return false;
+    }
 }
--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Customer {
+
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, double transaction) {
+        this.name = name;
+        this.transactions = new ArrayList<>();
+        transactions.add(transaction);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+
+    public void addTransaction(double transaction){
+        transactions.add(transaction);
+        System.out.println("The transaction list is: " + transactions);
+    }
 }
