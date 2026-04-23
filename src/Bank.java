16dea790ae4e601273773e87f1b0be6afb23ff84
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-04-23 11:54:44 +0100

    Updates

--- src/Bank.java
+++ src/Bank.java
@@ -3,4 +3,11 @@ import java.util.ArrayList;
 public class Bank {
     private String name;
     private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<>();
+    }
+
+
 }
